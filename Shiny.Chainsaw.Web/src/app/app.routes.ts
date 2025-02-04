5116962ae165eb85c7d31a9b709cde216321e12d
Mario Vega <maariovc@gmail.com>
2025-02-04 12:57:11 -0700

    Login component created.

--- Shiny.Chainsaw.Web/src/app/app.routes.ts
+++ Shiny.Chainsaw.Web/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CheckinComponent } from './checkin/checkin.component';
 import { CustomersComponent } from './customers/customers.component';
 import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: CheckinComponent },
     { path: 'customers', component: CustomersComponent },
     { path: 'users', component: UsersComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
--- Shiny.Chainsaw.Web/src/app/header/header.component.ts
+++ Shiny.Chainsaw.Web/src/app/header/header.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatNavList, MatListItem } from '@angular/material/list';
-import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatNavList, MatListItem, MatMenu, MatMenuItem, MatMenuTrigger],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
